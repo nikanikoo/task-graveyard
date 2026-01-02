bbcf0b648bcd00ce3d7951e3eda863f212c77862
Nika <me@nikori.ru>
2026-01-02 21:55:07 +0300

    edit file: math.py

--- python/math.py
+++ python/math.py
@@ -2,34 +2,38 @@ while (True):
     try:
         a = float(input("Введите первое число: "))
         b = float(input("Введите второе число: "))
-        p = input("Введите операцию (+, -, *, /, ^): ")
+        p = input("Введите операцию (+, -, *, /, ^, %): ")
     except ValueError:
         print("Ошибка: Введите корректные числовые значения!")
         continue
 
     if p == "+":
-        print("Результат:", a + b)
+        print(f"Результат: {a + b}")
 
     elif p == "-":
-        print("Результат:", a - b)
+        print(f"Результат: {a - b}")
 
     elif p == "*":
-        print("Результат:", a * b)
+        print(f"Результат: {a * b}")
 
     elif p == "/":
         if b != 0:
-            print("Результат:", a / b)
+            print(f"Результат: {a / b}")
         else:
             print("Ошибка: Деление на ноль невозможно!")
 
     elif p == "^":
-        print("Результат:", pow(a, b))
+        print(f"Результат: {pow(a, b)}")
+
+    elif p == "%":
+        if b != 0:
+            print(f"Результат: {a % b}")
+        else:
+            print("Ошибка: Деление на ноль невозможно!")
 
     else:
         print("Ошибка: Неизвестная операция!")
 
     cont = input("Хотите продолжить? (y/n): ").lower()
-    if cont == 'y':
-        continue
-    else:
+    if cont != 'y':
         break
